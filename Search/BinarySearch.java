aa90459136cbea213f27d379f673267f31da56b9
Avinash Vundyala <vundyala.avinash@gmail.com>
2016-07-12 01:16:10 +0530

    binary search

--- Search/BinarySearch.java
+++ Search/BinarySearch.java
@@ -4,12 +4,28 @@ public class BinarySearch {
         int midInx = (int) Math.ceil((end + start) / 2.0f); // Remember type casting.
         if(arr[midInx] == n) {
             return midInx;
-        } else if (start == end) {
+        } else if (start <= end) {
             return -1;
         } else if (arr[midInx] > n) {
-            return recursiveBinarySearch(arr, n, start, midInx);
+            return recursiveBinarySearch(arr, n, start, midInx - 1);
         } else if (arr[midInx] < n) {
-            return recursiveBinarySearch(arr, n, midInx, end);
+            return recursiveBinarySearch(arr, n, midInx + 1, end);
+        }
+        return -1;
+    }
+    
+    public int iterationBinarySearch(int[] arr, int n) {
+        boolean notFound = true;
+        int midInx, start = 0, end = arr.length - 1;
+        while(start < end) {
+            midInx = (int) Math.ceil((end + start) / 2.0f);
+            if(arr[midInx] == n) {
+                return midInx;
+            } else if (arr[midInx] > n) {
+                end = midInx - 1;
+            } else {
+                start = midInx + 1;
+            }
         }
         return -1;
     }
@@ -28,9 +44,11 @@ public class BinarySearch {
         BinarySearch bs = new BinarySearch();
 
         bs.printIndex(n, bs.recursiveBinarySearch(arr, n, 0, arr.length-1));
+        bs.printIndex(n, bs.iterationBinarySearch(arr, n));
 
         n = 11; 
         bs.printIndex(n, bs.recursiveBinarySearch(arr, n, 0, arr.length-1));
+        bs.printIndex(n, bs.iterationBinarySearch(arr, n));
     }
 
 
